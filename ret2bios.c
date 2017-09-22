56fcf93343f1009aa934ce4b3769b2246a69542e
Liam P. White <inkscapebrony@gmail.com>
2017-09-22 17:53:39 -0400

    use symbols that exist

--- ret2bios.c
+++ ret2bios.c
@@ -1,7 +1,26 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
-#include <linux/reboot.h>
+#include <linux/cpu.h>
+
+static void migrate_to_reboot_cpu(void)
+{
+	// The boot cpu is always logical cpu 0
+	int cpu = 0;
+
+	cpu_hotplug_disable();
+
+	// Make certain the cpu I'm about to reboot on is online
+	if (!cpu_online(cpu))
+		cpu = cpumask_first(cpu_online_mask);
+
+	// Prevent races with other tasks migrating this task
+	current->flags |= PF_NO_SETAFFINITY;
+
+	// Make certain I only run on the appropriate processor
+	set_cpus_allowed_ptr(current, cpumask_of(cpu));
+}
+
 
 static char ret2bios_long[] = "crash";
 static char ret2bios_real[] = "crash";
@@ -16,9 +35,9 @@ static int __init ret2bios_init(void)
 
     migrate_to_reboot_cpu();
 
-    ret2long_loc = xlate_dev_mem_ptr(0x10000);
-    ret2real_loc = xlate_dev_mem_ptr(0x7e00);
-    ret2boot_loc = xlate_dev_mem_ptr(0x7c00);
+    ret2long_loc = phys_to_virt(0x10000);
+    ret2real_loc = phys_to_virt(0x7e00);
+    ret2boot_loc = phys_to_virt(0x7c00);
 
     memcpy(ret2long_loc, ret2bios_long, sizeof ret2bios_long);
     memcpy(ret2real_loc, ret2bios_real, sizeof ret2bios_real);
